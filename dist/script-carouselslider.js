eec15cdf2a507150c63fb8c95ada37b85bcb0103
jamesschein <james@schein.co.uk>
2022-11-17 19:13:15 +0000

    Return to Sanity

--- dist/script-carouselslider.js
+++ dist/script-carouselslider.js
@@ -35,17 +35,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   placeButtons();
 
-  const reload = () => {
-    carouselSlider.style.transition = "none";
-    size = window.clientWidth;
-    carouselSlider.style.transform = "translateX(" + -size * counter + "px)";
-  };
-
-  reload();
-
   // RESIZE - RESET CAROUSEL
   window.addEventListener("resize", () => {
-    reload();
+    carouselSlider.style.transition = "none";
+    size = carouselCards[0].clientWidth;
+    carouselSlider.style.transform = "translateX(" + -size * counter + "px)";
+
     placeButtons();
   });
 
