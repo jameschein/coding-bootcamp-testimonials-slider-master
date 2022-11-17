214987fd16515ab6f417f5cb4a3e3f2c7118cecb
jamesschein <james@schein.co.uk>
2022-11-17 21:34:51 +0000

    JS Clean up

--- dist/script-carouselslider.js
+++ dist/script-carouselslider.js
@@ -2,7 +2,7 @@ const api_url = "data.json";
 const profiles = [];
 const imageArray = [];
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   const profileName = document.querySelectorAll(".profile-name");
   const profileJob = document.querySelectorAll(".profile-job");
   const profileQuote = document.querySelectorAll(".profile-quote");
@@ -14,7 +14,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const data = await response.json();
     profiles = data.profiles;
 
-    for (let i = 0; i < profileName.length; i++) {
+    let i = 0;
+
+    for (i = 0; i < profileName.length; i += 1) {
       profileQuote[i].children[0].textContent = profiles[i].quote[0];
       profileQuote[i].children[1].textContent = profiles[i].quote[1];
       profileJob[i].textContent = profiles[i].job;
@@ -24,25 +26,15 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   callAPI();
-
   /* END DATA GETTING APP */
 
   // JS THE BUTTON BLOC SO IT'S ALWAYS AT THE END OF THE IMAGE
-  const placeButtons = () => {
+  const placeButtons = function () {
     const btnBlk = document.querySelector(".button-block");
     btnBlk.style.top = profilePic[0].clientWidth * 1.025 + "px";
   };
 
   placeButtons();
-
-  // RESIZE - RESET CAROUSEL
-  window.addEventListener("resize", () => {
-    carouselSlider.style.transition = "none";
-    size = carouselCards[0].clientWidth;
-    carouselSlider.style.transform = "translateX(" + -size * counter + "px)";
-
-    placeButtons();
-  });
 
   // MECHANISM - GET THE PARTS
   const carouselSlider = document.querySelector(".slider");
@@ -50,65 +42,82 @@ document.addEventListener("DOMContentLoaded", () => {
   //BUTTONS
   const prevBtn = document.querySelector(".prev");
   const nextBtn = document.querySelector(".next");
-  //COUNTER
-  let counter = 1;
+  //c
+  let c = 1;
   // GET WIDTH OF THE DEFAULT CARD
-  let size = window.innerWidth;
+  let sz = window.innerWidth;
+
+  // RESIZE - RESET CAROUSEL
+  window.addEventListener("resize", function () {
+    carouselSlider.style.transition = "none";
+    sz = carouselCards[0].clientWidth;
+    carouselSlider.style.transform = "translateX(" + -sz * c + "px)";
+
+    placeButtons();
+  });
 
   // document.querySelector(".posCheck").value = size;
 
   // START ON SECOND SLIDE
-  carouselSlider.style.transform = "translateX(" + -size * counter + "px)";
+  carouselSlider.style.transform = "translateX(" + -sz * c + "px)";
 
-  const buttonControl = (counterChange) => {
+  const buttonControl = function (cChange) {
     carouselSlider.style.transition = "transform 200ms ease-in";
-    counterChange;
-    carouselSlider.style.transform = "translateX(" + -size * counter + "px)";
-    return counter;
+    if (cChange === "plus") {
+      c += 1;
+    } else if (cChange === "minus") {
+      c -= 1;
+    }
+    carouselSlider.style.transform = "translateX(" + -sz * c + "px)";
+    return c;
   };
 
-  const sliderShift = (cardCalc) => {
+  const sliderShift = function (cardCalc) {
     carouselSlider.style.transition = "none";
-    counter = cardCalc;
-    carouselSlider.style.transform = "translateX(" + -size * counter + "px)";
+    c = cardCalc;
+    carouselSlider.style.transform = "translateX(" + -sz * c + "px)";
   };
 
   //BUTTON LISTENERS
-  nextBtn.addEventListener("click", () => {
-    if (counter >= carouselCards.length - 1) return;
-    buttonControl(counter++);
+  nextBtn.addEventListener("click", function () {
+    if (c >= carouselCards.length - 1) {
+      return;
+    }
+    buttonControl("plus");
   });
 
-  prevBtn.addEventListener("click", () => {
-    if (counter <= 0) return;
-    buttonControl(counter--);
+  prevBtn.addEventListener("click", function () {
+    if (c <= 0) {
+      return;
+    }
+    buttonControl("minus");
   });
 
-  document.addEventListener(
-    "keyup",
-    (e) => {
-      const name = e.key;
-      // const code = e.code;
-      //console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
+  document.addEventListener("keyup", function (e) {
+    const name = e.key;
+    // const code = e.code;
+    //console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
 
-      if (name === "ArrowRight") {
-        if (counter >= carouselCards.length - 1) return;
-        buttonControl(counter++);
-      } else if (name === "ArrowLeft") {
-        if (counter <= 0) return;
-        buttonControl(counter--);
+    if (name === "ArrowRight") {
+      if (c >= carouselCards.length - 1) {
+        return;
       }
-    },
-    false
-  );
+      buttonControl("plus");
+    } else if (name === "ArrowLeft") {
+      if (c <= 0) {
+        return;
+      }
+      buttonControl("minus");
+    }
+  });
 
-  carouselSlider.addEventListener("transitionend", () => {
-    if (carouselCards[counter].id === "lastClone") {
+  carouselSlider.addEventListener("transitionend", function () {
+    if (carouselCards[c].id === "lastClone") {
       //SLIDES RIGHT
       sliderShift(carouselCards.length - 2);
-    } else if (carouselCards[counter].id === "firstClone") {
+    } else if (carouselCards[c].id === "firstClone") {
       //SLIDES LEFT
-      sliderShift(carouselCards.length - counter);
+      sliderShift(carouselCards.length - c);
     }
   });
 });
